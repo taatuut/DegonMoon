749ea1a4924170de93e372bf32981fe68fb72bb5
Emil Zegers <e.zegers@upcmail.nl>
2020-06-09 18:27:49 +0200

    Renaming files.

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 /**
  * 
- * @summary Uses MongooseJS to Connect to MongoDB and write connection status to console
+ * @summary Uses MongooseJS to Connect to MongoDB and lists all collections within
  * @author taatuut
  *
  */
 
- require('dotenv').config();
+require('dotenv').config();
 
 let mongoose = require('mongoose'),
-    ATLAS_CONNECTION = 'mongodb+srv://'+process.env.ATLAS_USERNAME+':'+process.env.ATLAS_PASSWORD+'@'+process.env.ATLAS_CLUSTER+'/'+process.env.ATLAS_DATABASE,
-    CONNECTION_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useUnifiedTopology: true };
+  ATLAS_CONNECTION = 'mongodb+srv://'+process.env.ATLAS_USERNAME+':'+process.env.ATLAS_PASSWORD+'@'+process.env.ATLAS_CLUSTER+'/'+process.env.ATLAS_DATABASE,
+  CONNECTION_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useUnifiedTopology: true };
 
-mongoose.connect(ATLAS_CONNECTION, CONNECTION_OPTIONS, () => { console.log("[+] Succesfully connected to database."); });
-mongoose.disconnect();
+mongoose.connect(ATLAS_CONNECTION, CONNECTION_OPTIONS)
+
+mongoose.connection.on('open', function(){
+  mongoose.connection.db.listCollections().toArray(function(error, items) {
+    if (error) {
+      throw new Error(error);
+    } else {
+     console.log(items);
+     mongoose.disconnect();
+    }
+  });
+});
+
+mongoose.connection.on('error', function(error){
+  throw new Error(error);
+});
 
 /* EOF */
