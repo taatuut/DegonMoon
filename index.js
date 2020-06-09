266b9f280d7299924f7aeafcf945d39ae224abc8
Emil Zegers <e.zegers@upcmail.nl>
2020-06-09 12:20:44 +0200

    Initial code and configuration.

---
+++ index.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://dbAdam:dbHalsema2020%23@cluster0-1pg7l.azure.mongodb.net/test', () => { console.log("[+] Succesfully connected to database."); });
