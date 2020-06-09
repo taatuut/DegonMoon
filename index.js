0248488e8cbb52f75fc963fea558f9fd0ff87c8a
Emil Zegers <e.zegers@upcmail.nl>
2020-06-09 14:28:16 +0200

    More sensible variable names. Additional Mongoose deprecation options.

--- index.js
+++ index.js
@@ -8,9 +8,9 @@
  require('dotenv').config();
 
 let mongoose = require('mongoose'),
-    ATLAS_CONN = 'mongodb+srv://'+process.env.ATLAS_USERNAME+':'+process.env.ATLAS_PASSWORD+'@'+process.env.ATLAS_CLUSTER+'/'+process.env.ATLAS_DATABASE,
-    ATLAS_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
+    ATLAS_CONNECTION = 'mongodb+srv://'+process.env.ATLAS_USERNAME+':'+process.env.ATLAS_PASSWORD+'@'+process.env.ATLAS_CLUSTER+'/'+process.env.ATLAS_DATABASE,
+    CONNECTION_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useUnifiedTopology: true };
 
-mongoose.connect(ATLAS_CONN, ATLAS_OPTIONS, () => { console.log("[+] Succesfully connected to database."); });
+mongoose.connect(ATLAS_CONNECTION, CONNECTION_OPTIONS, () => { console.log("[+] Succesfully connected to database."); });
 
 /* EOF */
--- listCollections.js
+++ listCollections.js
@@ -8,10 +8,10 @@
 require('dotenv').config();
 
 let mongoose = require('mongoose'),
-    ATLAS_CONN = 'mongodb+srv://'+process.env.ATLAS_USERNAME+':'+process.env.ATLAS_PASSWORD+'@'+process.env.ATLAS_CLUSTER+'/'+process.env.ATLAS_DATABASE,
-    ATLAS_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
+  ATLAS_CONNECTION = 'mongodb+srv://'+process.env.ATLAS_USERNAME+':'+process.env.ATLAS_PASSWORD+'@'+process.env.ATLAS_CLUSTER+'/'+process.env.ATLAS_DATABASE,
+  CONNECTION_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useUnifiedTopology: true };
 
-mongoose.connect(ATLAS_CONN, ATLAS_OPTIONS)
+mongoose.connect(ATLAS_CONNECTION, CONNECTIONS_OPTIONS)
 
 mongoose.connection.on('open', function(){
   mongoose.connection.db.listCollections().toArray(function(error, items) {
